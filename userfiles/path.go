7d6d82877be3ec39700a50f1c1b8ebddd89b37e2
zzzhr <zzzhr@hotmail.com>
2019-03-27 19:24:08 +0800

    update

---
+++ userfiles/path.go
@@ -0,0 +1,37 @@
+package userfiles
+
+import (
+	"path/filepath"
+	"strings"
+
+	gString "github.com/zzzhr1990/go-server-common/strings"
+)
+
+// FormatPath to format windows path like unix path
+func FormatPath(path string) string {
+	cleanStr := filepath.Clean(strings.Replace(strings.Trim(path, " \r\n\t"), "\\", "/", -1))
+	if cleanStr == "." {
+		return "/"
+	}
+	if strings.HasPrefix(cleanStr, "..") {
+		return FormatPath("/" + cleanStr)
+	}
+	if !strings.HasPrefix(cleanStr, "/") {
+		return "/" + cleanStr
+	}
+	return cleanStr
+}
+
+// getIdentity calc a file's uuid.
+func getIdentity(path string, ignoreCase bool) string {
+	if ignoreCase {
+		return gString.MD5String(strings.ToLower(path))
+	}
+	return gString.MD5String(path)
+}
+
+// GetFormatedIdentity calc a file's identity.
+func GetFormatedIdentity(path string, ignoreCase bool) (string, string) {
+	formated := FormatPath(path)
+	return getIdentity(formated, ignoreCase), formated
+}
---
+++ userfiles/path_test.go
@@ -0,0 +1,38 @@
+package userfiles_test
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+	util "github.com/zzzhr1990/go-server-common/userfiles"
+)
+
+func TestFormatPath(t *testing.T) {
+	Convey("Test Format", t, func() {
+		So(util.FormatPath(""), ShouldEqual, "/")
+		So(util.FormatPath("/"), ShouldEqual, "/")
+		So(util.FormatPath("/\\"), ShouldEqual, "/")
+		So(util.FormatPath("\\"), ShouldEqual, "/")
+		So(util.FormatPath("/a/.."), ShouldEqual, "/")
+		So(util.FormatPath("/../jj"), ShouldEqual, "/jj")
+		So(util.FormatPath("/../我爱你"), ShouldEqual, "/我爱你")
+		So(util.FormatPath("我爱你"), ShouldEqual, "/我爱你")
+		So(util.FormatPath("我\\爱你"), ShouldEqual, "/我/爱你")
+		So(util.FormatPath("c/dc/d/c/dc/d/cd/cd/cdc/d/"), ShouldEqual, "/c/dc/d/c/dc/d/cd/cd/cdc/d")
+		So(util.FormatPath("////////////"), ShouldEqual, "/")
+		So(util.FormatPath("//////..//////"), ShouldEqual, "/")
+		So(util.FormatPath("..//////"), ShouldEqual, "/")
+		So(util.FormatPath("../abc"), ShouldEqual, "/abc")
+		So(util.FormatPath("../abc "), ShouldEqual, "/abc")
+		So(util.FormatPath(" xnewu"), ShouldEqual, "/xnewu")
+		So(util.FormatPath(" xnewu.j"), ShouldEqual, "/xnewu.j")
+		sx, _ := util.GetFormatedIdentity("/abc", true)
+		So(sx, ShouldEqual, "482a7143ac747eff5e5a5992a6016d65")
+		sx, _ = util.GetFormatedIdentity("/Abc", true)
+		So(sx, ShouldEqual, "482a7143ac747eff5e5a5992a6016d65")
+		sx, _ = util.GetFormatedIdentity("/Abc", false)
+		So(sx, ShouldEqual, "a3189deb2541249c196149967a5a2a11")
+		sx, _ = util.GetFormatedIdentity("/////Abc", false)
+		So(sx, ShouldEqual, "a3189deb2541249c196149967a5a2a11")
+	})
+}
