4726cf01b6488d5cc94e54a51f76d4fdb51e10ce
zzzhr <zzzhr@hotmail.com>
2019-04-19 20:55:22 +0800

    gosc

---
+++ servername/constants.go
@@ -0,0 +1,10 @@
+package servername
+
+const (
+	//FileRPCServiceName file
+	FileRPCServiceName = "go-user-file"
+	//UserRPCServiceName file
+	UserRPCServiceName = "go-user-center"
+	//CloudSroreRPCServiceName file
+	CloudSroreRPCServiceName = "go-cloud-store"
+)
