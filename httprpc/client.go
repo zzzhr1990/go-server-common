168f09b2e53cf21ee73beaddb33ba93782b35401
zzzhr <zzzhr@hotmail.com>
2019-04-24 20:26:14 +0800

    add api request

---
+++ httprpc/client.go
@@ -0,0 +1,61 @@
+package httprpc
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+//PostJSONAndUnMarshal post json
+func PostJSONAndUnMarshal(url string, postData interface{}, recvData interface{}) error {
+	data, err := json.Marshal(postData)
+	if err != nil {
+		log.Errorf("Cannot Marshal json %v", err)
+		return err
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	// req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Errorf("Http Api request err %v", err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Errorf("Decode Http body err %v", err)
+		return err
+	}
+	err = json.Unmarshal(responseBody, recvData)
+	if err != nil {
+		log.Errorf("Http Api request Unmarshal error %v", err)
+		return err
+	}
+	return nil
+}
+
+//TryPostJSONAndUnMarshal post json
+func TryPostJSONAndUnMarshal(url string, postData interface{}, recvData interface{}, tryTime int) error {
+	//success := false
+	for {
+		err := PostJSONAndUnMarshal(url, postData, recvData)
+		if err == nil {
+			return nil
+		}
+		//
+		tryTime--
+		if tryTime < 1 {
+			return err
+		}
+		log.Errorf("Request API %v error %v, retrying", url, err)
+		time.Sleep(2 * time.Second)
+	}
+}
