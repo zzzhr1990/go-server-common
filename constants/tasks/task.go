668f07ff8833618bf34517012e39543cda036d9b
zzzhr <zzzhr@hotmail.com>
2019-04-26 16:01:43 +0800

    .D

--- constants/tasks/task.go
+++ constants/tasks/task.go
@@ -3,4 +3,23 @@ package tasks
 const (
 	//FileMimeDetect detect file mime
 	FileMimeDetect int32 = 1001
+	// OfflineDownloadDetect detect
+	// Detect Unkown Download Task
+	OfflineDownloadDetect int32 = 5001
+
+	// OfflineDownloadURL URL Only
+	// URL type, http/https prifix
+	OfflineDownloadURL int32 = 5020
+
+	// OfflineDownloadTorrent Torrent Only
+	// URL type, http/https prifix
+	OfflineDownloadTorrent int32 = 5030
+
+	// OfflineDownloadThunder thunder
+	OfflineDownloadThunder int32 = 5040
+
+	// OfflineDownloadMagnet magnet
+	OfflineDownloadMagnet int32 = 5050
+	// OfflineDownloadEd2k ED2K
+	OfflineDownloadEd2k int32 = 5060
 )
