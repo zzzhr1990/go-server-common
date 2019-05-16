ba4e6faabf4128f295996ff5975ad8e4e1859297
zzzhr <zzzhr@hotmail.com>
2019-05-16 22:20:09 +0800

    add new path

--- userfiles/path.go
+++ userfiles/path.go
@@ -1,7 +1,7 @@
 package userfiles
 
 import (
-	"path/filepath"
+	// "path/filepath"
 	"strings"
 
 	// github.com/zzzhr1990/go-server-common/userfiles
@@ -10,16 +10,34 @@ import (
 
 // FormatPath to format windows path like unix path but
 func FormatPath(path string) string {
-	cleanStr := filepath.Clean(strings.Replace(strings.Trim(path, " \r\n\t"), "\\", "/", -1))
-	if cleanStr == "." {
-		return "/"
+	// cleanStr := strings.Replace(path, ".", "", -1)
+	cleanStr := strings.Replace(strings.Trim(path, " \r\n\t"), "\\", "/", -1)
+
+	// split every
+	s := strings.Split(cleanStr, "/")
+	var sb strings.Builder
+
+	for _, ss := range s {
+		// sb.WriteString("/")
+		clear := strings.Trim(ss, " \r\n\t")
+		if clear != ".." && clear != "." && len(clear) > 0 {
+			sb.WriteString("/")
+			sb.WriteString(clear)
+		}
 	}
-	if strings.HasPrefix(cleanStr, "..") {
-		return FormatPath("/" + cleanStr)
-	}
+
+	cleanStr = sb.String()
+
+	/*
+		if strings.HasSuffix(cleanStr, "/") {
+			cleanStr = cleanStr[:len(cleanStr)-1]
+		}
+	*/
+
 	if !strings.HasPrefix(cleanStr, "/") {
 		return "/" + cleanStr
 	}
+
 	return cleanStr
 }
 
